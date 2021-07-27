e67607e6881652f6b461980ad4998ed3a316126d
Ed Tansey <office.fwd@yahoo.com>
2021-07-27 15:05:03 +0100

    Override and protext scope

--- classes/ListingPremium.php
+++ classes/ListingPremium.php
@@ -2,6 +2,7 @@
 
 class ListingPremium extends ListingBasic
 {
+  	protected $status = 'premium';
   	private $description;
   	protected $allowed_tags = '<p><br><b><strong><em><u><ol><ul><li>';
   
@@ -9,7 +10,6 @@ class ListingPremium extends ListingBasic
     * Gets the local property $description
     * @return string
     */
-
     public function getDescription()
     {
         return $this->description;
@@ -19,7 +19,6 @@ class ListingPremium extends ListingBasic
     * Cleans up and sets the local property $title
     * @param string $value to set property
     */
-    
     public function setDescription($value)
     {
         $this->description = trim(strip_tags($value, $this->allowed_tags));
